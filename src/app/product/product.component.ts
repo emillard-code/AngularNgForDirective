cb58982aa2344d8996411e24c69450a18a0cbb69
Emerson Millard <emillard.code@gmail.com>
2022-08-20 12:00:00 -0700

    Added additional list.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,6 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   products : any;
+  productsNew : any = [
+    {
+      "id": 4387,
+      "name": "watch",
+      "brand": "Timex",
+      "price": 7000
+    }, {
+      "id": 3877,
+      "name": "Mobile",
+      "brand": "One Plus",
+      "price": 55000
+    }, {
+      "id": 8938,
+      "name": "Book",
+      "brand": "head first",
+      "price": 999
+    }, {
+      "id": 3780,
+      "name": "T-Shirt",
+      "brand": "US POLO",
+      "price": 1699
+    }, {
+      "id": 5899,
+      "name": "AC",
+      "brand": "LG",
+      "price": 30000
+    }
+  ]
 
   constructor() { 
 
@@ -43,6 +71,9 @@ export class ProductComponent implements OnInit {
   }
 
   ngOnInit() {
+
+    // You can add the list elements here as well.
+
   }
 
 }
